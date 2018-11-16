556c3010dc012bac84cb8cd45bcc49a254d740a3
Gregory Antonovsky <onlinex0688@gmail.com>
2018-11-16 21:56:28 +0300

    Update ConsoleApplicationTree.cpp

--- ConsoleApplicationTree/ConsoleApplicationTree.cpp
+++ ConsoleApplicationTree/ConsoleApplicationTree.cpp
@@ -715,6 +715,10 @@ int main(int argc, char** argv)
 		else if (returnString == "f") {
 			A.search();
 		}
+		else if (returnString == "o") {
+			point* p = new point(window.mouse.x-512, -window.mouse.y+512, 10);
+			m.addpoint(m.root, p);
+		}
 		if (A.getEndNode() == true) {
 			do {
 				ret = A.search();
